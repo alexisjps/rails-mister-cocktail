a470d255f8fa58c95f8d307d97f3c96ba942f163
alexis <alexis@hellomybusiness.fr>
2020-10-03 17:32:56 +0200

    à terminer les vues presque finies + manque style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+puts 'all cocktails destroyed'
+lemon = Ingredient.create!(name: "lemon")
+ice = Ingredient.create!(name: "ice")
+mint_leaves = Ingredient.create!(name: "mint leaves")
+puts 'ingredient bien créé'
+# cocktails et doses
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+mojito = Cocktail.create!(name: "Mojito")
+pignacolada = Cocktail.create!(name: "Pignacolada")
+orange_juice = Cocktail.create!(name: "Orange Juice")
+puts 'cocktail créé'
+Dose.create!(cocktail: mojito, ingredient: lemon, description: '2cl vodkha')
+Dose.create!(cocktail: pignacolada, ingredient: mint_leaves, description: 'Ananas saveur')
+Dose.create!(cocktail: orange_juice, ingredient: ice, description: 'Frais et pressé maison')
+puts 'dose créée'
