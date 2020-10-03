0a73c5a5f571856dae6781fdb2feb3820b7efbaf
alexis <alexis@hellomybusiness.fr>
2020-10-03 14:58:08 +0200

    commit

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,2 +1,5 @@
 class Cocktail < ApplicationRecord
+  has_many :doses, :ingredients
+  validates :name, uniqueness: true, presence: true
+  validates :doses, presence: true
 end
--- db/migrate/20201003110612_create_cocktails.rb
+++ db/migrate/20201003110612_create_cocktails.rb
@@ -1,7 +1,8 @@
-class CreateCocktail < ActiveRecord::Migration[6.0]
+class CreateCocktails < ActiveRecord::Migration[6.0]
   def change
     create_table :cocktails do |t|
       t.string :name
+
       t.timestamps
     end
   end
