da166278dceff64b8335d4af3d4527f9dfc14919
alexis <alexis@hellomybusiness.fr>
2020-10-08 20:34:10 +0200

    modifs

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,19 +1,25 @@
-class DosesController < ApplicationController
- def create
+class DoseController < ApplicationController
+def create
     @cocktail = Cocktail.find(params[:cocktail_id])
-    @review = Review.new(review_params)
-    @review.cocktail = @cocktail
-    if @review.save
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
       redirect_to cocktail_path(@cocktail)
     else
-      @dose = Dose.new
+      @review = Review.new
       render "cocktails/show"
     end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
   private
 
-  def review_params
-    params.require(:review).permit(:rating, :content)
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
